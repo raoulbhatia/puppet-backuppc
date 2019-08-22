4c4b5d4cd8507cdbbef73f4d73407f1532196857
mergwyn <gary@theclarkhome.com>
2019-08-22 20:47:11 +0100

    More work on coverage reports

--- spec/spec_helper_local.rb
+++ spec/spec_helper_local.rb
@@ -1,37 +1,7 @@
-require 'coveralls'
-Coveralls.wear!
-
-require 'simplecov'
-require 'simplecov-console'
-require 'codecov'
-
-SimpleCov.formatters = [
-  SimpleCov::Formatter::HTMLFormatter,
-  SimpleCov::Formatter::Console,
-  SimpleCov::Formatter::Codecov,
-]
-SimpleCov.start do
-  #track_files 'lib/**/*.rb'
-
-  add_filter '/spec'
-
-  # do not track vendored files
-  add_filter '/vendor'
-  add_filter '/.vendor'
-
-  # do not track gitignored files
-  # this adds about 4 seconds to the coverage check
-  # this could definitely be optimized
-  add_filter do |f|
-    # system returns true if exit status is 0, which with git-check-ignore means file is ignored
-    system("git check-ignore --quiet #{f.filename}")
-  end
+if ENV["SIMPLECOV"]
+  require "simplecov"
+  SimpleCov.start { add_filter "/spec/" }
+elsif ENV["TRAVIS"] && RUBY_VERSION.to_f >= 1.9
+  require "coveralls"
+  Coveralls.wear! { add_filter "/spec/" }
 end
-
-RSpec.configure do |c|
-  # Coverage generation
-  c.after(:suite) do
-    RSpec::Puppet::Coverage.report!
-  end
-end
-
