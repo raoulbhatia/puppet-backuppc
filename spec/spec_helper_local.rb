39a74572b054a06e831caacbc89501156a682308
mergwyn <gary@theclarkhome.com>
2019-08-22 20:49:39 +0100

    Fix lint error

--- spec/spec_helper_local.rb
+++ spec/spec_helper_local.rb
@@ -1,7 +1,7 @@
-if ENV["SIMPLECOV"]
-  require "simplecov"
-  SimpleCov.start { add_filter "/spec/" }
-elsif ENV["TRAVIS"] && RUBY_VERSION.to_f >= 1.9
-  require "coveralls"
-  Coveralls.wear! { add_filter "/spec/" }
+if ENV['SIMPLECOV']
+  require 'simplecov'
+  SimpleCov.start { add_filter '/spec/' }
+elsif ENV['TRAVIS'] && RUBY_VERSION.to_f >= 1.9
+  require 'coveralls'
+  Coveralls.wear! { add_filter '/spec/' }
 end
