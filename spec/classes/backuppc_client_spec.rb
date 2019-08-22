7d7b6916384035aadfe297a5669f59541000f9b6
mergwyn <gary@theclarkhome.com>
2019-08-22 17:27:46 +0100

    Remove failure to compile test

--- spec/classes/backuppc_client_spec.rb
+++ spec/classes/backuppc_client_spec.rb
@@ -1,12 +1,6 @@
 require 'spec_helper'
 
 describe 'backuppc::client' do
-  describe 'On an unknown operating system' do
-    let(:facts) { { 'os' => { 'family' => 'Unknown', 'name' => 'Unknown' } } }
-
-    it { is_expected.to compile.and_raise_error(%r{is not supported by this module}) }
-  end
-
   on_supported_os.each do |os, facts|
     context "on #{os}" do
       let(:facts) { facts }
