e7f180f39919f5820245ad15c42881e86a39c5c1
mergwyn <gary@theclarkhome.com>
2019-09-11 17:25:07 +0100

    Add check for client class

--- spec/classes/backuppc_client_spec.rb
+++ spec/classes/backuppc_client_spec.rb
@@ -6,6 +6,7 @@ describe 'backuppc::client' do
       let(:facts) { facts }
       let(:params) { { 'backuppc_hostname' => 'backuppc.test.com' } }
 
+      it { is_expected.to contain_class('backuppc::client') }
       it { is_expected.to contain_class('backuppc::params') }
       it { is_expected.to compile.with_all_deps }
     end
