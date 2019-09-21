6763569a522adb47c6ddd3b30436cc7d0a154542
mergwyn <gary@theclarkhome.com>
2019-09-21 14:52:29 +0100

    Paths don't have to be absolute - revert to string

--- types/sharename.pp
+++ types/sharename.pp
@@ -3,4 +3,4 @@
 #   List of shares (used in other types).
 #   This can be set to a string or an array of strings.
 #
-type Backuppc::ShareName = Variant[Stdlib::Absolutepath,Array[Stdlib::Absolutepath]]
+type Backuppc::ShareName = Variant[String,Array[String]]
