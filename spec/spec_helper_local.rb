eb34d856a43cc087a139af990672a42ea12142ba
mergwyn <gary@theclarkhome.com>
2019-10-01 19:33:08 +0100

    Add require 'puppetlabs_spec_helper/module_spec_helper'

--- spec/spec_helper_local.rb
+++ spec/spec_helper_local.rb
@@ -9,6 +9,7 @@ SimpleCov.formatters = [
 SimpleCov.start do
   add_filter '/spec/'
 end
+require 'puppetlabs_spec_helper/module_spec_helper'
 
 # method to convert between puppet and backuppc names, and to convert values
 def extractkey(tparam)
