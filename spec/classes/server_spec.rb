b9bb09a74ee31f17e9234ca63f02e9093ad29535
mergwyn <gary@theclarkhome.com>
2019-10-01 18:53:17 +0100

    Increase coverage to 100%

--- spec/classes/server_spec.rb
+++ spec/classes/server_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe 'backuppc::server' do
   on_supported_os.each do |os, facts|
+    let(:node) { 'testhost.test.com' }
     context "on #{os}" do
       let(:facts) { facts }
 
@@ -10,18 +11,23 @@ describe 'backuppc::server' do
         when 'RedHat'
           { group_apache: 'apache',
             config_directory: '/etc/BackupPC',
-            topdir: '/var/lib/BackupPC' }
+            topdir: '/var/lib/BackupPC'
+          }
         when 'Debian'
           { group_apache: 'www-data',
             config_directory: '/etc/backuppc',
-            topdir: '/var/lib/backuppc' }
+            topdir: '/var/lib/backuppc',
+            preseed_file: '/var/cache/debconf/backuppc.seeds',
+          }
         end
       end
-      let(:command) do
+      let(:htpasswd_command) do
         "test -f #{options[:config_directory]}/htpasswd   || OPT='-c';htpasswd -bs ${OPT}   #{options[:config_directory]}/htpasswd backuppc 'test_password'"
       end
 
-      default_params = { backuppc_password: 'test_password' }
+      default_params = {
+        backuppc_password: 'test_password',
+      }
 
       context 'with defaults' do
         let(:params) { default_params }
@@ -49,6 +55,17 @@ describe 'backuppc::server' do
         it { is_expected.to contain_file('topdir_ssh').with_ensure('directory') }
         it { is_expected.to contain_file('apache_config').with_ensure('present') }
         it { is_expected.to contain_backuppc__server__user('backuppc').with_password('test_password') }
+        it { is_expected.to contain_exec(htpasswd_command).with_command(htpasswd_command) }
+
+        it { is_expected.to contain_exec('backuppc-ssh-keygen').with_command(
+          "ssh-keygen -f #{options[:topdir]}/.ssh/id_rsa -C 'BackupPC on #{node}' -N ''"
+          )}
+
+        case facts[:os]['family']
+        when 'Debian'
+          it { is_expected.to contain_file(options[:preseed_file]).with_ensure('present') } 
+        end
+
         # Workaround for imported resourcses
         case facts[:os]['family']
         when 'RedHat'
@@ -56,55 +73,67 @@ describe 'backuppc::server' do
         when 'Debian'
           it { is_expected.to contain_file('/etc/BackupPC').with_ensure('link') }
         end
+      end
 
-        it { is_expected.to contain_exec(command).with_command(command) }
+      context 'config.pl content' do
+        test_params = {
+          'wakeup_schedule' => [1, 2, 3, 4],
+          'max_backups' => 3,
+          'max_user_backups' => 5,
+          'language' => 'de',
+          'max_pending_cmds' => 12,
+          'max_backuppc_nightly_jobs' => 5,
+          'backuppc_nightly_period' => 2,
+          'max_old_log_files' => 7,
+          'df_max_usage_pct' => 89,
+          'trash_clean_sleep_sec' => 299,
+          'full_period' => 13.97,
+          'full_keep_cnt' => [4, 2, 3],
+          'full_age_max' => 89,
+          'incr_period' => 0.57,
+          'incr_keep_cnt' => 5,
+          'incr_age_max' => 29,
+          'incr_levels' => [1, 2, 3],
+          'incr_fill' => true,
+          'partial_age_max' => 2,
+          'restore_info_keep_cnt' => 11,
+          'archive_info_keep_cnt' => 11,
+          'blackout_good_cnt' => 5,
+          'cgi_url' => 'http://localhost/backuppc/',
+          'backup_zero_files_is_fatal' => false,
+          'email_notify_min_days' => 2.1,
+          'email_from_user_name' => 'backup_user',
+          'email_admin_user_name' => 'backup_user',
+          'email_user_dest_domain' => '@example.com',
+          'email_notify_old_backup_days' => 11,
+          'cgi_image_dir_url' => '/images',
+          'cgi_admin_users' => 'backup',
+          'cgi_admin_user_group' => 'backup',
+          'cgi_date_format_mmdd' => 2,
+          'user_cmd_check_status' => false,
+          'ping_max_msec' => 4,
+        }
+        test_params.each do |tparam, tvalue|
+          context "with #{tparam} = #{tvalue}" do
+            let(:params) { default_params.merge(tparam => tvalue) }
+
+            content = config_content(tparam, tvalue)
+            it { is_expected.to contain_file('config.pl').with_content(content) }
+          end
+        end
       end
 
       test_params = {
-        'wakeup_schedule' => [1, 2, 3, 4],
-        'max_backups' => 3,
-        'max_user_backups' => 5,
-        'language' => 'de',
-        'max_pending_cmds' => 12,
-        'max_backuppc_nightly_jobs' => 5,
-        'backuppc_nightly_period' => 2,
-        'max_old_log_files' => 7,
-        'df_max_usage_pct' => 89,
-        'trash_clean_sleep_sec' => 299,
-        'full_period' => 13.97,
-        'full_keep_cnt' => [4, 2, 3],
-        'full_age_max' => 89,
-        'incr_period' => 0.57,
-        'incr_keep_cnt' => 5,
-        'incr_age_max' => 29,
-        'incr_levels' => [1, 2, 3],
-        'incr_fill' => true,
-        'partial_age_max' => 2,
-        'restore_info_keep_cnt' => 11,
-        'archive_info_keep_cnt' => 11,
-        'blackout_good_cnt' => 5,
-        'cgi_url' => 'http://localhost/backuppc/',
-        'backup_zero_files_is_fatal' => false,
-        'email_notify_min_days' => 2.1,
-        'email_from_user_name' => 'backup_user',
-        'email_admin_user_name' => 'backup_user',
-        'email_user_dest_domain' => '@example.com',
-        'email_notify_old_backup_days' => 11,
-        'cgi_image_dir_url' => '/images',
-        'cgi_admin_users' => 'backup',
-        'cgi_admin_user_group' => 'backup',
-        'cgi_date_format_mmdd' => 2,
-        'user_cmd_check_status' => false,
-        'ping_max_msec' => 4,
+        'apache_configuration' => false,
       }
       test_params.each do |tparam, tvalue|
         context "with #{tparam} = #{tvalue}" do
           let(:params) { default_params.merge(tparam => tvalue) }
-
-          content = config_content(tparam, tvalue)
-          it { is_expected.to contain_file('config.pl').with_content(content) }
+          it { is_expected.not_to contain_file('apache_config') }
+          it { is_expected.not_to contain_backuppc__server__user('backuppc') }
         end
       end
+
     end
   end
 end
